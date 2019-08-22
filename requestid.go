f41d89ce0f75022bddeea91db48d7c7341f5597a
Kirill Samoilov <kreisnews@gmail.com>
2019-08-22 17:18:35 +1000

    Add logrus support

--- requestid.go
+++ requestid.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
 )
 
 // ContextKey is
@@ -16,6 +17,9 @@ const (
 
 	// HTTPHeaderNameRequestID has the name of the header for request ID
 	HTTPHeaderNameRequestID = "X-Request-ID"
+
+	// LogFieldKeyReqID is the logfield key for RequestID
+	LogFieldKeyReqID = "requestId"
 )
 
 // GetReqID will get reqID from a http request and return it as a string
@@ -51,6 +55,24 @@ func Middleware(next http.Handler) http.Handler {
 		h.Add(HTTPHeaderNameRequestID, GetReqID(ctx))
 
 		next.ServeHTTP(w, r)
-
 	})
+}
+
+// NewLoggerFromReqIDStr creates  a *logrus.Entry that has requestID as a field. A new LogField inst will be created if log is nil
+func NewLoggerFromReqIDStr(reqID string, ancestorLogger logrus.FieldLogger) logrus.FieldLogger {
+
+	var retLogger logrus.FieldLogger = logrus.StandardLogger()
+
+	if ancestorLogger != nil {
+		retLogger = ancestorLogger
+	}
+
+	return retLogger.WithField(LogFieldKeyReqID, reqID)
+}
+
+// NewLoggerFromReqIDCtx creates a *logrus.Entry that has requestID as a field.  A new LogField inst will be created if log is ni
+func NewLoggerFromReqIDCtx(ctx context.Context, ancestorLogger logrus.FieldLogger) logrus.FieldLogger {
+	reqID := GetReqID(ctx)
+
+	return NewLoggerFromReqIDStr(reqID, ancestorLogger)
 }
